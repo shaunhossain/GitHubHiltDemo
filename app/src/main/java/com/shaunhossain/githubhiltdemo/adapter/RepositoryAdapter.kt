c45c8658d27fa58f0d453ac2b06cb120eb834011
shaunhossain <shaunhossain95@gmail.com>
2020-09-26 18:37:03 +0600

    A demo application for learning dagger hilt . it's a beginner level demo .

--- app/src/main/java/com/shaunhossain/githubhiltdemo/adapter/RepositoryAdapter.kt
+++ app/src/main/java/com/shaunhossain/githubhiltdemo/adapter/RepositoryAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.shaunhossain.githubhiltdemo.R
 import com.shaunhossain.githubhiltdemo.model.GitHubDataModelItem
 import kotlinx.android.synthetic.main.item_repository_preview.view.*
@@ -46,7 +47,21 @@ class RepositoryAdapter : RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
         val gitHubDataModelItem = differ.currentList[position]
 
         holder.itemView.apply {
-            reposDesc.text = gitHubDataModelItem.description
+            try {
+                Glide.with(this)
+                    .load(gitHubDataModelItem.owner.avatarUrl)
+                    .override(100, 70)
+                    .placeholder(R.drawable.error_thum)
+                    .error(R.drawable.error_thum)
+                    .centerCrop()
+                    .into(user_avatar)
+            } catch (t : Throwable){
+                user_avatar.setImageResource(R.drawable.error_thum)
+            }
+
+            user_name.text = gitHubDataModelItem.owner.login
+            project_name.text = "Repos : ${gitHubDataModelItem.fullName}"
+            project_description.text = "Description : ${gitHubDataModelItem.description}"
         }
 
         }
